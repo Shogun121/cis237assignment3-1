ba2b4db4de2c11ec2d2fca3cc5341096e32e0ed8
Robert Cooley <Daimyo121@aol.com>
2016-10-11 22:08:26 -0400

    1)Created Droid collection class. 2)(Work in Progress) Creating the droid creation method interface.

---
+++ cis237assignment3/DroidCollection.cs
@@ -0,0 +1,31 @@
+﻿/**
+ * Robert Cooley
+ * */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237assignment3
+{   //class used to store the droid array and perform various tasks on it.
+    class DroidCollection
+    {
+        //----------
+        //Backing Fields
+        //----------
+
+        //----------
+        //Constructor
+        //----------
+
+        //----------
+        //Methods
+        //----------
+
+        //----------
+        //Properties
+        //----------
+
+    }
+}
--- cis237assignment3/UserInterface.cs
+++ cis237assignment3/UserInterface.cs
@@ -15,6 +15,7 @@ namespace cis237assignment3
         //Backing Fields
         //-----------------------
         string userInput;
+        bool droidTypeValidation;
         //-----------------------
         //Methods
         //-----------------------
@@ -43,15 +44,22 @@ namespace cis237assignment3
         {   
                 switch (userInput)
                 {
-                    case "1": //Print the droid list.
+                    case "1": 
+                        //Print the droid list.
                         Console.WriteLine("You have selected Option 1");
                         GetMainInput();
                         break;
-                    case "2": //Add a Droid to the list.
-                        Console.WriteLine("You have selected Option 2");
+                    case "2": 
+                        //Add a Droid to the list.
+                        Console.WriteLine("You have selected 'Add a Droid'");
+                        GetDroidType();
+
+                        //Allow the user to continue making deicisions.
+                        Console.WriteLine("What would you like to do now?");
                         GetMainInput();
                         break;
-                    case "3": //Exit the system.
+                    case "3": 
+                        //Exit the system.
                         Console.WriteLine("Thank you for using the Droid Management System.");
                         break;
                 }   
@@ -92,9 +100,7 @@ namespace cis237assignment3
             Console.WriteLine();
             Console.WriteLine("Invalid Input, please enter a number '1' through '3' for an option.");
             Console.WriteLine("The input you gave was "+ "'"+userInput+"'");
-            Console.WriteLine();
-
-            
+            Console.WriteLine();     
         }
         /// <summary>
         /// A method used to run the UI cleanly.
@@ -118,6 +124,65 @@ namespace cis237assignment3
         public void ClearConsole()
         {
             Console.Clear();
+        }
+        /// <summary>
+        /// Method used to acquire user input to determine what droid type to create.
+        /// </summary>
+        public void GetDroidType()
+        {   //delcare bool as false to validate.
+            droidTypeValidation = false;
+
+            //loop while bool remains false.
+            while(droidTypeValidation==false)
+            {
+                //Stores a line of input in the 'Input' property.
+                Input = Console.ReadLine();
+                try
+                {
+                    //Compares 'Input' to the droid types.
+                    if (Input.ToLower() == "protocol" && Input.ToLower() == "utility"
+                        && Input.ToLower() == "janitor" && Input.ToLower() == "astromech")
+                    {
+                        //If 'Input' matches one of the droid types the switcher is called.
+                        CreateDroid();
+                    }
+                    //Data has been validated, bool is set to true.
+                    droidTypeValidation = true;
+                }
+                catch
+                {
+                    //Method call to inform the user that the input is invalid.
+                    InvalidDroidType();
+                }
+            }
+            
+        }
+        /// <summary>
+        /// method used to switch through the droid types to 
+        /// correctly prompt the user to create a droid.
+        /// </summary>
+        public void CreateDroid()
+        {
+            switch(Input)
+            {
+                case "protocol"://call CreateProtocolDroid()
+                    break;
+                case "utility"://call CreateUtilityDroid()
+                    break;
+                case "janitor"://call CreateJanitorDroid()
+                    break;
+                case "astromech"://call CreateAstromechDroid()
+                    break;
+            }
+        }
+        /// <summary>
+        /// method used to report incorrect droid types.
+        /// </summary>
+        public void InvalidDroidType()
+        {
+            Console.WriteLine();
+            Console.WriteLine("The droid type '{0}' is not valid.",Input);
+            Console.WriteLine("Please enter a valid droid type.");
         }
         //-----------------------
         //Properties
